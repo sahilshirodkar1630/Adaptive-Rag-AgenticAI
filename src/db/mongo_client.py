fc0cd2cc3bf9afa3d1cdf8fb72301248ce162887
Sahil Shirodkar <sahilshirodkar1630@gmail.com>
2026-06-10 14:23:59 +0530

    added mongo db integration

--- src/db/mongo_client.py
+++ src/db/mongo_client.py
@@ -1,10 +1,12 @@
 """
 MongoDB client initialization.
 """
-
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
-MONGO_URL = "mongodb://localhost:27017"
+#MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 DB_NAME = "adaptive_rag"
 
 client = AsyncIOMotorClient(MONGO_URL)
