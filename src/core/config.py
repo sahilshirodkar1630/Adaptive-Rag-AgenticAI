cc8bfa8b5218b9a518d41ecb67b18b9c838a261a
Sahil Shirodkar <sahilshirodkar1630@gmail.com>
2026-06-10 14:23:45 +0530

    added config changes

--- src/core/config.py
+++ src/core/config.py
@@ -13,6 +13,7 @@ class Settings:
     """Application settings loaded from environment variables."""
 
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
+    GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
     TAVILY_API_KEY: str = os.getenv("TAVILY_API_KEY", "")
     QDRANT_URL = os.getenv("QDRANT_URL")
     QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
@@ -25,3 +26,4 @@ settings = Settings()
 # Set env variables for LangChain integrations
 os.environ["OPENAI_API_KEY"] = settings.OPENAI_API_KEY
 os.environ["TAVILY_API_KEY"] = settings.TAVILY_API_KEY
+os.environ["GROQ_API_KEY"] = settings.GROQ_API_KEY
